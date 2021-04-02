598bd0e376913708ae868baa2fbaa0f7546f895c
Вячеслав <lass.tx@yandex.ru>
2021-04-02 19:28:16 +0300

    final

--- api/urls.py
+++ api/urls.py
@@ -13,7 +13,7 @@ router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet,
                 basename='comments')
 
 urlpatterns = [
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('v1/', include(router.urls)),
 ]
